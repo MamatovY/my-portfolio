842f469ab7dbe189dd2f1c7f1031679698fe032b
MamatovY <mamatovyiman28@gmail.com>
2023-09-13 17:57:35 +0600

    fix experience

--- components/experience.tsx
+++ components/experience.tsx
@@ -18,7 +18,7 @@ export default function Experience() {
   return (
     <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>Мой опыт</SectionHeading>
-      <VerticalTimeline lineColor="">
+      <VerticalTimeline className="overflow-hidden" lineColor="">
         {experiencesData.map((item, index) => (
           <React.Fragment key={index}>
             <VerticalTimelineElement
