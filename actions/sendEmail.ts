05a71709e6d1eb4868dc936f0601c33e21ae5237
MamatovY <mamatovyiman28@gmail.com>
2023-09-13 15:41:24 +0600

    fix api-key

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -5,7 +5,7 @@ import { Resend } from "resend";
 import { validateString, getErrorMessage } from "@/lib/utils";
 import ContactFormEmail from "@/email/contact-form-email";
 
-const resend = new Resend('re_KB99ih9o_NbYTgrYEX27uuK3U3TwfKUYf');
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
