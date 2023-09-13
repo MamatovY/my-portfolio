a7813e6a9b60c46c47636efbec59aede020906a8
MamatovY <mamatovyiman28@gmail.com>
2023-09-13 16:41:01 +0600

    delete overflow-hidden

--- components/experience.tsx
+++ components/experience.tsx
@@ -18,7 +18,7 @@ export default function Experience() {
   return (
     <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>Мой опыт</SectionHeading>
-      <VerticalTimeline className="overflow-hidden" lineColor="">
+      <VerticalTimeline lineColor="">
         {experiencesData.map((item, index) => (
           <React.Fragment key={index}>
             <VerticalTimelineElement
